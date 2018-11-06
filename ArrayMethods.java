1a098a4b99dc993a8be62a439b06b46a4d44d50a
vlu00 <vlu00@stuy.edu>
2018-11-06 18:21:16 -0500

    Added and tested allRowSums and allColSums

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -1,5 +1,5 @@
 public class ArrayMethods {
-  public static int rowSum (int [] [] ary, int x) {
+  public static int rowSum (int [][] ary, int x) {
     int sum = 0;
     if (x > ary.length -1 || x < 0) {
       return sum;
@@ -10,24 +10,46 @@ public class ArrayMethods {
     return sum;
   }
 
-  public static int columnSum(int [][] ary, int x) {
+  public static int Great(int[][] ary) {
     int greatest = 0;
     for (int i = 0; i < ary.length; i++) {
       if (ary[i].length > greatest) {
         greatest = ary[i].length;
       }
     }
-    if (x > greatest || x < 0) {
+    return greatest;
+  }
+
+  public static int columnSum(int [][] ary, int x) {
+    if (x > Great(ary) || x < 0) {
       return 0;
     }
     else {
-    int sum = 0;
-    for (int i = 0; i < ary.length; i++) {
-      if (ary[i].length > x) {
-        sum = sum + ary[i][x];
+      int sum = 0;
+      for (int i = 0; i < ary.length; i++) {
+        if (ary[i].length > x) {
+          sum = sum + ary[i][x];
+        }
       }
+      return sum;
     }
-    return sum;
   }
-}
+
+  public static int[] allRowSums(int[][] ary){
+    int [] sums = new int [ary.length];
+    for (int i = 0; i < ary.length; i++) {
+      sums[i] = rowSum(ary, i);
+    }
+    return sums;
+  }
+
+  public static int[] allColSums(int[][] ary){
+    int x = Great(ary);
+    int [] sums = new int [x];
+    for (int i = 0; i < x; i++) {
+      sums[i] = columnSum(ary, i);
+    }
+    return sums;
+  }
+
 }
--- Driver.java
+++ Driver.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Driver {
   public static void main (String [] args) {
     int [][] A = new int [][] {
@@ -21,5 +22,19 @@ public class Driver {
     System.out.println(ArrayMethods.columnSum(B, 3));
     System.out.println(ArrayMethods.columnSum(B, 2));
     System.out.println(ArrayMethods.columnSum(B, 1));
+
+    int [][] C = new int [][] {
+      new int[] {1,2,3,4},
+      new int[] {2,2,2},
+      new int[] {3,4},
+      new int[] {7},
+    };
+    System.out.println(Arrays.toString(ArrayMethods.allRowSums(A)));
+    System.out.println(Arrays.toString(ArrayMethods.allRowSums(B)));
+    System.out.println(Arrays.toString(ArrayMethods.allRowSums(C)));
+    System.out.println(Arrays.toString(ArrayMethods.allColSums(A)));
+    System.out.println(Arrays.toString(ArrayMethods.allColSums(B)));
+    System.out.println(Arrays.toString(ArrayMethods.allColSums(C)));
+
   }
 }
